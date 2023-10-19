74523d2a9a9d4cd5bb31f63b7d2ba6ac8b014abf
Alek <alek.br@gmail.com>
2023-10-18 17:54:42 -0700

    Schedule update

--- js/schedule.js
+++ js/schedule.js
@@ -9,18 +9,9 @@ const schedule = {
 	 "time": "9:00 AM",
 	 "location": "Supplyframe HQ 3rd Floor"
 	},
-	// {
-	//  "speaker": "Reboot",
-	//  "title": "Lunch",
-	//  "about": "",
-	//  "description": "",
-	//  "date": "Friday, November 4, 2022",
-	//  "time": "12:00 PM",
-	//  "location": "Supplyframe HQ 3rd Floor"
-	// },
 	{
 	 "speaker": "Matt Venn",
-	 "title": "Tiny Tapeout - Design Your Own Chip and Get it Made!",
+	 "title": "Tiny Tapeout - Design Your Own Chip and Get it Made! (Workshop #1)",
 	 "about": "Matt Venn is a science & technology communicator and electronic engineer. He brings 20 years of engineering experience to create innovative learning experiences for people all over the world. Most recently he has been focusing on the world of open source silicon.",
 	 "description": "In this workshop, you can design and manufacture your own chip on an ASIC. You will learn the basics of digital logic, how semiconductors are made, the skills needed to use an online digital design tool for simulation, and how to create the GDS file for manufacturing. Participants will also have the option to submit their designs to be manufactured as part of the Tiny Tapeout project.",
 	 "date": "Friday, November 3, 2023",
@@ -29,7 +20,7 @@ const schedule = {
 	},
 	{
 	 "speaker": "Matt Venn",
-	 "title": "Tiny Tapeout - Design Your Own Chip and Get it Made!",
+	 "title": "Tiny Tapeout - Design Your Own Chip and Get it Made!  (Workshop #2)",
 	 "about": "Matt Venn is a science & technology communicator and electronic engineer. He brings 20 years of engineering experience to create innovative learning experiences for people all over the world. Most recently he has been focusing on the world of open source silicon.",
 	 "description": "In this workshop, you can design and manufacture your own chip on an ASIC. You will learn the basics of digital logic, how semiconductors are made, the skills needed to use an online digital design tool for simulation, and how to create the GDS file for manufacturing. Participants will also have the option to submit their designs to be manufactured as part of the Tiny Tapeout project.",
 	 "date": "Friday, November 3, 2023",
@@ -75,6 +66,15 @@ const schedule = {
 	 "location": "LACM"
 	},		
 	{
+	 "speaker": "Andrew Denio",
+	 "title": "Automate Your Workflow for Tindie and other Marketplaces",
+	 "about": "",
+	 "description": "Andrew (aka MakeItHackin) is a hardware enthusiast, maker, and tinkerer. He became involved with #BadgeLife two years ago and enjoys creating blinky badges yearly for conferences. His talk discusses creating and selling on marketplaces like Tindie, providing valuable advice for those seeking to sell their own hardware projects.",
+	 "date": "Saturday, November 4, 2023",
+	 "time": "10:00 AM - 11:20 AM",
+	 "location": "DesignLab"
+	},					
+	{
 	 "speaker": "Eric Andersson",
 	 "title": "A Makers Guide To Designing Sheet Metal Electronics Enclosures",
 	 "about": "",
@@ -91,6 +91,51 @@ const schedule = {
 	 "date": "Saturday, November 4, 2023",
 	 "time": "01:00 PM - 01:20 PM",
 	 "location": "LACM"
+	},
+	{
+	 "speaker": "Nathan Jones",
+	 "title": "Inside the Voja4",
+	 "about": "",
+	 "description": "Nathan Jones is an active-duty Army officer and Assistant Professor at West Point Military Academy. He earned his bachelor's degree in Engineering from Harvey Mudd College and his master's degree in Computer Engineering from North Carolina State University. One of Nathan's favorite things to do is make neat electronic devices and then teach people how to do the same.",
+	 "date": "Saturday, November 4, 2023",
+	 "time": "01:00 PM - 01:40 PM",
+	 "location": "DesignLab"
+	},		
+	{
+	 "speaker": "Jakub and Szymon Duchniewicz",
+	 "title": "Porting an AI Powered Wearable Health Monitor to Zephyr on Open Hardware",
+	 "about": "",
+	 "description": "Jakub is an MSc in Embedded Systems graduate with a minor in Innovation and Entrepreneurship. He works as an engineer and team leader at Tietoevry, where he honed his low-level skills by developing L1 features for 5G NR. In this talk, audience members will discover how deploying AI models on hardware is more accessible than most people realize.",
+	 "date": "Saturday, November 4, 2023",
+	 "time": "01:30 PM - 02:10 PM",
+	 "location": "LACM"
+	},
+	{
+	 "speaker": "Programming Your Human Hardware Compilers, or the Road to Writing Great Step-by-Step Instructions",
+	 "title": "Joshua Vasquez",
+	 "about": "",
+	 "description": "At his day job, Joshua generally automates hardware with software and rolling out the occasional circuit board for ad-hoc scientific instruments. By night, he’s often tinkering with something with a strong tendency to either autonomously roll off my desk or drill holes in itself. In his talk, Joshua will share how to write step-by-step instructions in a way that builds community and avoids burnout. ",
+	 "date": "Saturday, November 4, 2023",
+	 "time": "02:00 PM - 02:40 PM",
+	 "location": "DesignLab"
+	},		
+	{
+	 "speaker": "Max Shirokawa",
+	 "title": "OpenQuantum: Open-source Atom Trapping For Education and Entrepreneurship",
+	 "about": "",
+	 "description": "Max is a graduate student in EECS, a CNC machinist, and an open-source maker. He enjoys working at the intersection of art, industrial design, optics, and materials science. In his talk, Max will demonstrate how quantum mechanics can be observed and controlled without needing expensive equipment or a mastery of the concepts.",
+	 "date": "Saturday, November 4, 2023",
+	 "time": "03:30 PM - 04:10 PM",
+	 "location": "DesignLab"
+	},		
+	{
+	 "speaker": "James Lewis",
+	 "title": "Building The Apple II Computer That Apple Intended But Never Did",
+	 "about": "",
+	 "description": "James Lewis is the Bald Engineer from the YouTube channel AddOhms and the host of element14 Presents. His interest includes talking too much about capacitors, repairing vintage computers, and making unique PCBs. In his talk, James will show the value of using older designs to learn new skills and feature tools that are useful when reverse engineering electronics. ",
+	 "date": "Saturday, November 4, 2023",
+	 "time": "04:30 PM - 05:10 PM",
+	 "location": "DesignLab"
 	},
 	{
 	 "speaker": "Aleksa Bjelogrlic",
@@ -159,6 +204,124 @@ const schedule = {
 	 "time": "09:30 AM",
 	 "location": "LACM"
 	},
+	{
+	 "speaker": "Thea Flowers",
+	 "title": "The Absurd Idea of Rendering KiCAD Projects on The Web",
+	 "about": "",
+	 "description": "Thea is an artist, engineer, writer, and open-source advocate. She creates synthesizers and builds engaging developer experiences. She's currently serving on the board of the Open Source Hardware Association, and she also has an unreasonable fondness for weasels. In her talk, Thea will show real-world applications of interactive web technology, curious facts about KiCAD, and more!",
+	 "date": "Sunday, November 5, 2023",
+	 "time": "10:30 AM - 11:10 AM",
+	 "location": "DesignLab"
+	},	
+	{
+	 "speaker": "Matt Venn",
+	 "title": "Weave Your Own Core Memory - Core16!",
+	 "about": "",
+	 "description": "This workshop provides you with the opportunity to weave your own core memory! Using 16 authentic ferrite core bits and 16 RGB LEDs, you can play tic-tac-toe, paint with a magnetic stylus, and create your own interactive experiences. Andy Geppert will guide you through the assembly of Core16. The Core16 kit is the little brother of the Core64 kit. The smaller Core16 kit reduces assembly time/cost, enabling more people to experience the challenge and satisfaction of creating their own core memory. ",
+	 "date": "Sunday, November 5, 2023",
+	 "time": "10:00 AM - 12:00 PM",
+	 "location": "Supplyframe HQ 2nd Floor"
+	},		
+	{
+	 "speaker": "David Prutchi",
+	 "title": "Receiving Microwave Signals from Deep-Space Probes – Amateur DSN and the Ultimate DX",
+	 "about": "",
+	 "description": "David Prutchi is dedicated to bringing cutting-edge experimental physics, technical photography, and space communications within the grasp of fellow science buffs. He holds a Ph.D. from Tel-Aviv University, and his area of expertise is developing active implantable medical devices. In his talk, David explores how deep space exploration creates new and exciting opportunities in ham radio.",
+	 "date": "Sunday, November 5, 2023",
+	 "time": "11:30 AM - 12:00 PM",
+	 "location": "DesignLab"
+	},					
+	{
+	 "speaker": "Tom Mloduchowski",
+	 "title": "Hack-own-airplane! Experimental/Amateur-Built aircraft and innovation",
+	 "about": "",
+	 "description": "Tom Mloduchowski is a seasoned electronics engineer and entrepreneur passionate about developing cutting-edge hardware and software solutions. He has spent over a decade working on various innovative projects, from blockchain-based payroll lending to tele-mental health startups. Tom is passionate about aviation and has, in the past, dabbled in general aviation maintenance, experimental aircraft, and electric propulsion. ",
+	 "date": "Sunday, November 5, 2023",
+	 "time": "01:00 PM - 01:40 PM",
+	 "location": "DesignLab"
+	},	
+	{
+	 "speaker": "Pierce Nichols",
+	 "title": "Teaching Robots To Sail",
+	 "about": "",
+	 "description": "Pierce is a long-time maker of electronic gizmos and robot boats. He's also built rockets and may have had something to do with the flappy bits on the wings of the last commercial jet you flew on. In his talk, Pierce explains how sailing works, how he went about automating it, and gives some insight into the technology development process.",
+	 "date": "Sunday, November 5, 2023",
+	 "time": "02:00 PM - 02:20 PM",
+	 "location": "LACM"
+	},		
+	{
+	 "speaker": "Jesse T. Gonzalez",
+	 "title": "Circuit Boards that Breathe and Bend (A Guide to Electro-Pneumatic Robotic Surfaces)",
+	 "about": "",
+	 "description": "Jesse builds robotic, shape-changing surfaces that enable adaptive environments and tangible user experiences. He’s a PhD candidate at Carnegie Mellon’s Human-Computer Interaction Institute and an off-and-on consultant at Alto Metric Studio. His talk covers pneumatic structures in PCBs that transform rigid boards into dynamic, compact, electro-mechanical devices. Discover how to build, control, and debug electrostatic air valves, using them to create soft robotic surfaces.",
+	 "date": "Sunday, November 5, 2023",
+	 "time": "02:00 PM - 02:20 PM",
+	 "location": "LACM"
+	},
+	{
+	 "speaker": "Kumar Abhishek",
+	 "title": "Circuit Boards that Breathe and Bend (A Guide to Electro-Pneumatic Robotic Surfaces)",
+	 "about": "",
+	 "description": "Kumar currently lives in India and works for Google as a Software Engineer. They have previously worked for Analog Devices as well. In their talk, they will show the intricacies and lesser-known elements of the PIO and DMA controller of the Raspberry Pi Pico.",
+	 "date": "Sunday, November 5, 2023",
+	 "time": "02:30 PM - 03:10 PM",
+	 "location": "LACM"
+	},
+	{
+	 "speaker": "Jose Angel Torres",
+	 "title": "Building a Junkyard Secure Phone in China",
+	 "about": "",
+	 "description": "Jose has spent the past five years navigating every square inch of the world's electronics markets in Huaqiangbei, Shenzhen, China. Jose is fluent in spoken Chinese, allowing him unprecedented access to second-hand cellphone purchasing and repair, IC refurbishment, PCBA assembly, complete product reverse-engineering, cloning, and chip cracking. His talk covers making everyday devices more secure and sourcing parts in China.",
+	 "date": "Sunday, November 5, 2023",
+	 "time": "02:00 PM - 02:20 PM",
+	 "location": "DesignLab"
+	},
+	{
+	 "speaker": "Marc Verdiell",
+	 "title": "Restoring the Apollo Guidance Computer",
+	 "about": "",
+	 "description": "Marc, also known as CuriousMarc from the CuriousMarc YouTube channel, specializes in restoring rare and notable vintage electronics. His projects include Apollo space electronics, the Xerox Alto computer, IBM mainframes, and HP instruments, to name a few. Marc’s talk will take the audience through the epic restoration of a genuine Apollo Guidance Computer, the revolutionary technology that guided man to the Moon and back.",
+	 "date": "Sunday, November 5, 2023",
+	 "time": "02:30 PM - 03:10 PM",
+	 "location": "DesignLab"
+	},			
+	{
+	 "speaker": "Shawn Hymel",
+	 "title": "Tiny Reinforcement Learning in Robotics",
+	 "about": "",
+	 "description": "Shawn is a machine learning DevRel engineer, instructor, and university program manager at Edge Impulse. He creates compelling technical videos, courses, and workshops around edge machine learning that inspire engineers of all skill levels. In his talk, Shawn will provide an overview of reinforcement learning, how it’s used in robotics, its shortcomings, and why we shouldn’t give up on classical control theory yet.",
+	 "date": "Sunday, November 5, 2023",
+	 "time": "03:30 PM - 3:50 PM",
+	 "location": "LACM"
+	},		
+	{
+	 "speaker": "Bob Hickman",
+	 "title": "The Bits and Bytes of Bringing Arcade Classics to Game Consoles",
+	 "about": "",
+	 "description": "Bob is a game industry veteran with many shipped titles across all major platforms. He was also a Disney Imagineer, working on high-profile interactive attractions for several years. He’s currently a technology director in the automotive AI space. His talk is about retro programming technology, making tools for reverse engineering, and getting things working without source code.",
+	 "date": "Sunday, November 5, 2023",
+	 "time": "03:30 PM - 3:50 PM",
+	 "location": "DesignLab"
+	},
+	{
+	 "speaker": "Angela Sheehan",
+	 "title": "Cuddly Companion Bots: A Whimsical Adventure",
+	 "about": "",
+	 "description": "Angela (Gella) is a maker, artist, and educator passionate about wearable technology, product design, and digital fabrication. She has been working with craft tech and wearables since 2005, worked in the education department at SparkFun Electronics, and is currently a Creative Technologist at a public library makerspace. Her talk shows how companion projects are a great project for everyone, not just programmers.",
+	 "date": "Sunday, November 5, 2023",
+	 "time": "04:00 PM - 04:40 PM",
+	 "location": "LACM"
+	},
+	{
+	 "speaker": "Kuba Tyszko",
+	 "title": "Sudo, Make Me a Coffee: Adventures in Reverse-Engineering a Commercial Coffee Machine",
+	 "about": "",
+	 "description": "Kuba is a natural-born hacker with a penchant for diving into encrypted software in ways it was never meant for. Kuba’s experience includes launching a farming tractor at six, designing their own electronics, and building bicycles. In their talk, they dive into the simplicity of commercial coffee machines and how access to the hardware makes it easy to reverse engineer the design. ",
+	 "date": "Sunday, November 5, 2023",
+	 "time": "04:00 PM - 04:20 PM",
+	 "location": "DesignLab"
+	},
+
 	{
 	 "speaker": "Bob Hickman",
 	 "title": "The Bits and Bytes of Bringing Arcade Classics to Game Consoles",
